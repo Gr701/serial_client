d42c22882ff7452a4ec4f4ede3ba8cc712444366
Gr701 <gr701@LAPTOP-M49K4PME.wlan>
2025-10-06 03:54:11 +0300

    5

--- src/device_manager.py
+++ src/device_manager.py
@@ -28,6 +28,6 @@ def find_connected_device(allowed_devices):
             for port in serial.tools.list_ports.comports():
                 if port.vid is not None and port.pid is not None:
                     print(f"port.vid={port.vid} candidate={candidate['vendorId']} port.pid={port.pid} candidate={candidate['productId']}")
-                    if port.vid == candidate['vendorId'] and port.pid == candidate['productId']:
+                    if f'{port.vid:x}' == candidate['vendorId'] and f'{port.pid:x}' == candidate['productId']:
                         print('Found device: ', candidate['name'])
                         return port.device
